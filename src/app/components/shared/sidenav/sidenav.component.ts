790e6a7b8ee2a05f5672191ea76f494edc917d2a
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2023-11-10 15:54:05 -0600

    SYC | Making sidenav dynamic and adding click listener on items

--- src/app/components/shared/sidenav/sidenav.component.ts
+++ src/app/components/shared/sidenav/sidenav.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
+  navigationItems = [
+    [
+      {
+        icon: 'cat-paws.svg',
+        type: 'logo',
+        isDraggable: false,
+        redirect: '/',
+      },
+      {
+        icon: 'cat-dav.svg',
+        type: 'icon',
+        isDraggable: true,
+        redirect: '/favs',
+      },
+      {
+        icon: 'cat-tendencies.svg',
+        type: 'icon',
+        isDraggable: true,
+        redirect: '/tendencies',
+      },
+    ],
+    [
+      {
+        icon: 'plus.svg',
+        type: 'icon',
+        isDraggable: true,
+        redirect: '/new-post',
+      },
+    ],
+    [
+      {
+        icon: 'user.svg',
+        type: 'icon',
+        isDraggable: true,
+        redirect: '/profile',
+      },
+      {
+        icon: 'language.svg',
+        type: 'icon',
+        isDraggable: true,
+      },
+    ],
+  ];
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClick(url: string | undefined) {
+    if (url) this.router.navigateByUrl(url);
   }
-
 }
