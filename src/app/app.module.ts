c34cf09191a572aaf74ce35410fbd19def300b69
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2023-11-27 18:32:04 -0600

    SYC | Card component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './components/shared/sidenav/sidenav.component';
+import { CardComponent } from './components/shared/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidenavComponent
+    SidenavComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
