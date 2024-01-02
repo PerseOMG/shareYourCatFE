732959a9499adba2064062a012dc2f29e391d391
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2024-01-01 19:07:25 -0600

    SYC | Rename card component to cat of the week component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './components/shared/sidenav/sidenav.component';
-import { CardComponent } from './components/shared/card/card.component';
+import { CatOfTheWeekComponent } from './components/shared/card/cat-of-the-week.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SidenavComponent,
-    CardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SidenavComponent, CatOfTheWeekComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
