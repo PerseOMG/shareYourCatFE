83f197da035e0be43c1df6b0d4434636ea677f4c
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2024-01-11 21:27:37 -0600

    SYC | posts-lists component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './components/shared/sidenav/sidenav.component';
 import { CatOfTheWeekComponent } from './components/layout/cat-of-the-week/cat-of-the-week.component';
+import { PostsListsComponent } from './components/layout/posts-lists/posts-lists.component';
 
 @NgModule({
-  declarations: [AppComponent, SidenavComponent, CatOfTheWeekComponent],
+  declarations: [AppComponent, SidenavComponent, CatOfTheWeekComponent, PostsListsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
