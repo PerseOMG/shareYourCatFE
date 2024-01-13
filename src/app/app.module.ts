9a069f5e563250c282c6484e65abdf7ab0efba8a
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2024-01-12 19:36:38 -0600

    SYC | posts-lists WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { SidenavComponent } from './components/shared/sidenav/sidenav.component';
 import { CatOfTheWeekComponent } from './components/layout/cat-of-the-week/cat-of-the-week.component';
 import { PostsListsComponent } from './components/layout/posts-lists/posts-lists.component';
+import { CreatePostComponent } from './components/layout/create-post/create-post.component';
 
 @NgModule({
-  declarations: [AppComponent, SidenavComponent, CatOfTheWeekComponent, PostsListsComponent],
+  declarations: [AppComponent, SidenavComponent, CatOfTheWeekComponent, PostsListsComponent, CreatePostComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/layout/posts-lists/posts-lists.component.ts
+++ src/app/components/layout/posts-lists/posts-lists.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IMAGE_MOCK } from 'src/assets/app-consts/IMAGE_MOCK';
 
 @Component({
   selector: 'app-posts-lists',
   templateUrl: './posts-lists.component.html',
-  styleUrls: ['./posts-lists.component.scss']
+  styleUrls: ['./posts-lists.component.scss'],
 })
 export class PostsListsComponent implements OnInit {
+  @Input() posts = [
+    {
+      postHeading: 'THIS IS A',
+      postDescription: 'POST',
+      image: IMAGE_MOCK,
+      totalLikes: 1021011,
+    },
+    {
+      postHeading: 'THIS IS A',
+      postDescription: 'POST',
+      image: IMAGE_MOCK,
+      totalLikes: 1021011,
+    },
+    {
+      postHeading: 'THIS IS A',
+      postDescription: 'POST',
+      image: IMAGE_MOCK,
+      totalLikes: 1021011,
+    },
+    {
+      postHeading: 'THIS IS A',
+      postDescription: 'POST',
+      image: IMAGE_MOCK,
+      totalLikes: 1021011,
+    },
+    {
+      postHeading: 'THIS IS A',
+      postDescription: 'POST',
+      image: IMAGE_MOCK,
+      totalLikes: 1021011,
+    },
+    {
+      postHeading: 'THIS IS A',
+      postDescription: 'POST',
+      image: IMAGE_MOCK,
+      totalLikes: 1021011,
+    },
+    {
+      postHeading: 'THIS IS A',
+      postDescription: 'POST',
+      image: IMAGE_MOCK,
+      totalLikes: 1021011,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
