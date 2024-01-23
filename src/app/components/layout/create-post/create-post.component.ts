a9b354d406a277527db7a9fff084761366688b9c
PerseoOMG <88952291+PerseOMG@users.noreply.github.com>
2024-01-23 17:55:08 -0600

    SYC | WIP: Create post card

--- src/app/components/layout/create-post/create-post.component.ts
+++ src/app/components/layout/create-post/create-post.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IMAGE_MOCK } from 'src/assets/app-consts/IMAGE_MOCK';
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
-  styleUrls: ['./create-post.component.scss']
+  styleUrls: ['./create-post.component.scss'],
 })
 export class CreatePostComponent implements OnInit {
+  @Input() postData = {
+    image: IMAGE_MOCK,
+    postedBy: 'PerseoDMG',
+    userId: 0,
+    date: new Date(),
+    post: 'Lol, look ad dis',
+    isLikedByUser: true,
+    likes: [
+      {
+        userId: 1,
+        userName: 'IsraCum',
+      },
+      {
+        userId: 2,
+        userName: 'Chris',
+      },
+    ],
+    comments: [
+      {
+        userId: 0,
+        userName: 'PerseoDMG',
+        comment: 'dis nuts',
+      },
+      {
+        userId: 0,
+        userName: 'IsraCum',
+        comment: 'Nice Dog',
+      },
+    ],
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onFav() {
+    this.postData.isLikedByUser = !this.postData.isLikedByUser;
   }
-
 }
